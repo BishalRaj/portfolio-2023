7678f71083bfae3df5db76a844629effaeffe58e
BishalRaj <bisal1shakya@gmail.com>
2023-04-22 11:44:39 +0100

    added cards

--- src/components/PersonalProjectComponent/PersonalProjects.tsx
+++ src/components/PersonalProjectComponent/PersonalProjects.tsx
@@ -4,33 +4,96 @@ import { Col, Row } from "react-bootstrap";
 
 const PersonalProjects = () => {
   return (
-    <Row className="section-personal-project mx-auto w-75 bg-warning">
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+    <Row className="section-personal-project mx-auto bg-warning">
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
-      <Col lg={4} className="bg-danger mx-auto px-2">
-        <div className="bg-light">adajshdsajghd</div>
+      <Col lg={4} className="bg-danger mx-auto px-1 py-2 ">
+        <div className=" project-card p-4 mx-auto bg-warning">
+          <div></div>{" "}
+          <div>
+            <p></p>
+            <p></p>
+          </div>{" "}
+          <div></div>
+        </div>
       </Col>
     </Row>
   );
