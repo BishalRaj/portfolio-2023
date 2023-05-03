d90f840ac94afb002097f1dda396f52f5c012feb
BishalRaj <bisal1shakya@gmail.com>
2023-05-03 13:46:44 +0100

    added hyperlink to folder

--- src/components/PersonalProjectComponent/PersonalProjects.tsx
+++ src/components/PersonalProjectComponent/PersonalProjects.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.scss";
 import { Col, Row } from "react-bootstrap";
 import { AiOutlineFolder } from "react-icons/ai";
+import { FiGithub } from "react-icons/fi";
 
 const PersonalProjects = () => {
   var projects = [];
@@ -10,13 +11,20 @@ const PersonalProjects = () => {
       <Col lg={4} className="bg-danger  px-1 py-2">
         <div className="project-card p-4 mx-auto bg-light ">
           <Row className="h-100 d-flex justify-content-between">
-            <Col lg={6}>
-              <a href="/">
-                <AiOutlineFolder size={45} />
-              </a>
-            </Col>
-            <Col lg={6} className="text-right">
-              link
+            <Col lg={12} className="d-flex height-fit-content">
+              <div className="w-50">
+                <a href="/">
+                  <AiOutlineFolder size={45} />
+                </a>
+              </div>
+              <div className="text-right w-50 d-flex align-items-end py-2 justify-content-end">
+                <a href="/">
+                  <FiGithub size={20} />
+                </a>
+                <a href="/">
+                  <FiGithub size={20} />
+                </a>
+              </div>
             </Col>
             <Col lg={12}>
               <h2>title</h2>
