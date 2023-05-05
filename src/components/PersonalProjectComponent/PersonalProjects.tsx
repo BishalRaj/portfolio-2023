dffb1fd3b914f12d4795c41d7b2de2f4913d2e4d
BishalRaj <bisal1shakya@gmail.com>
2023-05-05 12:23:32 +0100

    created a json file for project data and improved design

--- src/components/PersonalProjectComponent/PersonalProjects.tsx
+++ src/components/PersonalProjectComponent/PersonalProjects.tsx
@@ -3,46 +3,91 @@ import "./style.scss";
 import { Col, Row } from "react-bootstrap";
 import { CiFolderOn } from "react-icons/ci";
 import { FiGithub } from "react-icons/fi";
+import ProjectData from "../../data/projects.json";
+import { Link } from "react-router-dom";
 
 const PersonalProjects = () => {
-  var projects = [];
-  for (let index = 0; index < 14; index++) {
-    projects.push(
-      // <Col lg={4} className="bg-danger  px-1 py-2">
+  // var projects = [];
+  // for (let index = 0; index < 5; index++) {
+  //   projects.push(
+  //     // <Col lg={4} className="bg-danger  px-1 py-2">
+  //     <Col lg={4} className="  px-1 py-2">
+  //       <div className="project-card p-4 mx-auto bg-light-navy">
+  //         <Row className="h-100 d-flex justify-content-between">
+  //           <Col lg={12} className="d-flex height-fit-content">
+  //             <div className="w-50">
+  //               <a href="/">
+  //                 <CiFolderOn size={45} className="color-custom-green" />
+  //               </a>
+  //             </div>
+  //             <div className="text-right w-50 d-flex align-items-end py-2 justify-content-end project-link">
+  //               <a href="/" className="ml-1">
+  //                 <FiGithub size={20} />
+  //               </a>
+  //               <a href="/" className="ml-1">
+  //                 <FiGithub size={20} />
+  //               </a>
+  //             </div>
+  //           </Col>
+  //           <Col lg={12}>
+  //             <h5 className="text-light">Title</h5>
+  //             <p></p>
+  //           </Col>
+  //           <Col lg={12} className="project-skills height-fit-content">
+  //             <ul>
+  //               <li>react.js</li>
+  //               <li>react.js</li>
+  //               <li>react.js</li>
+  //             </ul>
+  //           </Col>
+  //         </Row>
+  //       </div>
+  //     </Col>
+  //   );
+  // }
+
+  var projects = ProjectData.map((data) => {
+    return (
       <Col lg={4} className="  px-1 py-2">
-        <div className="project-card p-4 mx-auto bg-light-navy">
-          <Row className="h-100 d-flex justify-content-between">
-            <Col lg={12} className="d-flex height-fit-content">
-              <div className="w-50">
-                <a href="/">
-                  <CiFolderOn size={45} className="color-custom-green" />
-                </a>
-              </div>
-              <div className="text-right w-50 d-flex align-items-end py-2 justify-content-end project-link">
-                <a href="/" className="ml-1">
-                  <FiGithub size={20} />
-                </a>
-                <a href="/" className="ml-1">
-                  <FiGithub size={20} />
-                </a>
-              </div>
-            </Col>
-            <Col lg={12}>
-              <h5 className="text-light">Title</h5>
-              <p>definition</p>
-            </Col>
-            <Col lg={12} className="project-skills height-fit-content">
-              <ul>
-                <li>react.js</li>
-                <li>react.js</li>
-                <li>react.js</li>
-              </ul>
-            </Col>
-          </Row>
-        </div>
+        <Link to={"#"} className="project-link">
+          <div className="project-card p-4 mx-auto bg-light-navy">
+            <Row className="h-100 d-flex justify-content-between">
+              {/* Column links ******************************************************************************** */}
+              <Col lg={12} className="d-flex height-fit-content">
+                <div className="w-50">
+                  <a href={data.link}>
+                    <CiFolderOn size={45} className="color-custom-green" />
+                  </a>
+                </div>
+                <div className="text-right w-50 d-flex align-items-end py-2 justify-content-end repo-link">
+                  <a href={data.github} className="ml-1">
+                    <FiGithub size={20} />
+                  </a>
+                  <a href="/" className="ml-1">
+                    <FiGithub size={20} />
+                  </a>
+                </div>
+              </Col>
+              {/* ---------------------------------------------------------------------------------------------- */}
+              <Col lg={12}>
+                <h5 className="color-slate-lightest project-title">
+                  {data.title}
+                </h5>
+                <p className="color-slate-light">{data.description}</p>
+              </Col>
+              <Col lg={12} className="project-skills height-fit-content w-100">
+                <ul>
+                  {data.technologies.map((element) => (
+                    <li>{element}</li>
+                  ))}
+                </ul>
+              </Col>
+            </Row>
+          </div>
+        </Link>
       </Col>
     );
-  }
+  });
 
   return (
     <Row className="section-personal-project mx-auto d-flex justify-content-center">
